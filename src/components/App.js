78faea22e0facd3eebb7c0c3b3d5785ac211aadf
Vera Weikel <vera.weikel@gmail.com>
2023-04-21 11:55:29 -0700

    Update App.js.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InventoryControl from "./InventoryControl";
+
+function App(){
+  
+  return (
+    <React.Fragment>
+      <InventoryControl />
+    </React.Fragment>
+  )
+}
+
+export default App;
